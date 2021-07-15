e5fa27ae340a9312c618417baa2e53679fb06ffb
antonjiang <andyjiangs13@gmail.com>
2021-07-14 23:34:16 -0700

    Added AWS post HTTP and JSON parsing

--- popup.js
+++ popup.js
@@ -13,13 +13,53 @@ document.addEventListener('DOMContentLoaded', function() {
         d = document;
 
         var f = d.createElement('form');
-        f.action = 'https://leetcode.requestcatcher.com/test';
+        f.action = 'https://yz8vxbajt1.execute-api.us-east-1.amazonaws.com/default/leetTogether';
         f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = Http.responseText;
-        f.appendChild(i);
+        
+        data = JSON.parse(Http.responseText);
+        var user_name = d.createElement('input');
+        user_name.type = 'hidden';
+        user_name.name = 'user_name';
+        user_name.value = data.user_name;
+        f.appendChild(user_name);
+      
+        var num_solved = d.createElement('input')
+        num_solved.type = 'hidden';
+        num_solved.name = 'num_solved';
+        num_solved.value = data.num_solved;
+        f.appendChild(num_solved);
+
+        var num_total = d.createElement('input')
+        num_total.type = 'hidden';
+        num_total.name = 'num_total';
+        num_total.value = data.num_total;
+        f.appendChild(num_total);
+
+        var prob_list = d.createElement('input')
+        prob_list.type = 'hidden';
+        prob_list.name = 'prob_list';
+
+        var list = [];
+        for (const prob of data.stat_status_pairs){
+          const value = {difficulty_level:prob.difficulty.level,
+           is_favor:prob.is_favor, 
+           paid_only:prob.paid_only, 
+           status:prob.status,
+           frontend_question_id: prob.stat.frontend_question_id,
+           question__title: prob.stat.question__title,
+           question__title_slug: prob.stat.question__title_slug,
+           question_id: prob.stat.question_id,
+           total_acs: prob.stat.total_acs,
+           total_submitted: prob.stat.total_submitted,
+          };
+          if (value.status != null){
+            list.push(value);
+          }
+          
+        }
+        prob_list.value = JSON.stringify(list);
+        f.appendChild(prob_list);
+
         d.body.appendChild(f);
         f.submit();
       }
